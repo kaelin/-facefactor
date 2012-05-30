db955068214545dabf430100a3a89b8c15a5cd38
Kaelin Colclasure <kaelin@acm.org>
2012-05-30 14:21:41 -0700

    Experiment with MSER features for locating eyes.

--- runstep.m
+++ runstep.m
@@ -2,17 +2,34 @@
 inputImage = facefactor.fetchInputImage(cam);
 bbox = step(faceDetector, inputImage);
 if ~isempty(bbox)
-    faceImage = imcrop(inputImage, bbox(1,:));
+    faceBox = bbox(1, :);
+    % Adjust for an apparent training error in the faceDetector.
+    faceBox(3:4) = faceBox(3:4) * 1.015;
+    faceImage = imcrop(inputImage, faceBox);
     faceImage = imresize(faceImage, [200 NaN]);
     faceImage = imcrop(faceImage, [20 0 159 200]);
-    eyesImage = imcrop(faceImage, [20 60 119 39]);
+    eyesImage = imcrop(faceImage, [20 58 119 39]);
     faceImage = imadjust(faceImage, stretchlim(eyesImage, [0.001 0.999]));
     faceImage = immultiply(im2double(faceImage), mask);
 else
     faceImage = mask;
 end
+
+eyesImage = imadjust(eyesImage);
+eyesImage = im2double(eyesImage);
+eyesBlank = mean(mean(eyesImage));
+eyesImage = eyesImage - eyesBlank;
+eyesImage = eyesImage .* eyesMask;
+eyesImage = eyesImage + eyesBlank;
+%% Detect MSER features
+regions = detectMSERFeatures(eyesImage, 'RegionAreaRange', [35 530], 'MaxAreaVariation', 0.2);
+levels = arrayfun(@(x, y) eyesImage(floor(y), floor(x)), regions.Centroid(:, 1), regions.Centroid(:, 2));
+regions = regions(levels < eyesBlank);
+
 subplot(2, 3, [1 2 4 5]); subimage(inputImage); axis off;
-subplot(2, 3, 3); subimage(eyesImage); axis off;
+subplot(2, 3, 3); subimage(eyesImage); hold on;
+plot(regions);
+hold off;
 subplot(2, 3, 6); subimage(faceImage); axis off;
 if rec.Training.isvalid
     [label, score] = rec.step(faceImage);
--- setup.m
+++ setup.m
@@ -1,6 +1,6 @@
 %% Camera
 if ~exist('cam')
-    cam = videoinput('macvideo', 1, 'YCbCr422_1280x720');
+    cam = videoinput('macvideo', 3, 'YCbCr422_1280x720');
     cam.ROIPosition = [290 20 700 700];
     cam.ReturnedColorSpace = 'grayscale';
     cam.FramesPerTrigger = 1;
@@ -17,3 +17,7 @@ faceDetector.MinSize = [170 170];
 faceDetector.MergeThreshold = 7;
 
 mask = facefactor.gaussianMask(200, 160);
+eyesMask = [facefactor.gaussianMask(40, 50) zeros(40, 20) facefactor.gaussianMask(40, 50)];
+
+%% Recognition
+rec = facefactor.Recognizer('rec-ex3.mat');
